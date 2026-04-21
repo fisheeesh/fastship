b5b08b986a43ba540aef258774e1430db7ce049b
fisheeesh <swanphyo444@gmail.com>
2026-04-21 15:10:07 +0700

    feat: implement login route

--- app/api/routers/seller.py
+++ app/api/routers/seller.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 
 from ..dependencies import SellerServiceDep
 from ..schemas.seller import SellerCreate, SellerRead
@@ -6,6 +9,20 @@ from ..schemas.seller import SellerCreate, SellerRead
 router = APIRouter(prefix="/seller", tags=["Seller"])
 
 
+### Register a seller
 @router.post("/signup", response_model=SellerRead)
 async def register_seller(seller: SellerCreate, service: SellerServiceDep):
     return await service.add(seller)
+
+
+### Login the seller
+@router.post("/login")
+async def login_seller(
+    request_form: Annotated[OAuth2PasswordRequestForm, Depends()],
+    service: SellerServiceDep,
+):
+    token = await service.login(request_form.username, request_form.password)
+    return {
+        "access_token": token,
+        "type": "jwt",
+    }
--- app/services/seller.py
+++ app/services/seller.py
@@ -1,7 +1,14 @@
+from datetime import datetime, timedelta
+
+from fastapi import HTTPException, status
+import jwt
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from passlib.context import CryptContext
+from sqlmodel import col
 from ..api.schemas.seller import SellerCreate
 from ..database.models import Seller
+from ..config import security_settings
 
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -12,6 +19,18 @@ class SellerService:
         self.session = session
 
     async def add(self, credentials: SellerCreate) -> Seller:
+        result = await self.session.execute(
+            select(Seller).where(col(Seller.email) == credentials.email),
+        )
+
+        existing_seller = result.scalar()
+
+        if existing_seller:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="This email has been registered. Try again with another email.",
+            )
+
         seller = Seller(
             **credentials.model_dump(exclude=["password"]),  # type: ignore
             password_hash=password_context.hash(credentials.password),
@@ -21,3 +40,39 @@ class SellerService:
         await self.session.refresh(seller)
 
         return seller
+
+    async def login(self, email: str, password: str) -> str:
+        # * Get seller from db with provided email
+        result = await self.session.execute(
+            select(Seller).where(col(Seller.email) == email),
+        )
+
+        seller = result.scalar()
+
+        # * Check if seller is existed or not
+        if seller is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Seller with provided email is not found.",
+            )
+
+        password_correct = password_context.verify(password, seller.password_hash)
+
+        # * Check password is correct or not
+        if not password_correct:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Incorrect password. Please try again!",
+            )
+
+        # * If all passed, generate token
+        token = jwt.encode(
+            payload={
+                "user": {"name": seller.name, "email": seller.email},
+                "exp": datetime.now() + timedelta(days=1),
+            },
+            algorithm=security_settings.JWT_ALGORITHM,
+            key=security_settings.JWT_SECRET,
+        )
+
+        return token
