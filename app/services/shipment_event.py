26af52fdf29c4746543343c7aa45a2f0178420ff
fisheeesh <swanphyo444@gmail.com>
2026-05-04 00:27:09 +0700

    chore: attachecd review link via using token once shipment status is updated to delivered

--- app/services/shipment_event.py
+++ app/services/shipment_event.py
@@ -5,12 +5,15 @@ from uuid import UUID
 from sqlalchemy import delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from utils import generate_url_safe_token
+
 from ..database.redis import add_shipment_verification_code
 
 from .notification import NotificationService
 
 from ..database.models import Shipment, ShipmentEvent, ShipmentStatus
 from .base import BaseService
+from app.config import app_settings
 
 
 class ShipmentEventService(BaseService):
@@ -104,10 +107,13 @@ class ShipmentEventService(BaseService):
                 else:
                     context = {"verification_code": code}
             case ShipmentStatus.delivered:
+                token = generate_url_safe_token({"id":str(shipment.id)})
+
                 subject = "Your Order is Delivered ✅"
                 template_name = "mail_delivered.html"
                 context = {
                     "seller": shipment.seller.name,
+                    "review_url": f"http://{app_settings.APP_DOMAIN}/shipment/review?token={token}",
                 }
             case ShipmentStatus.cancelled:
                 subject = "Your Order Was Cancelled"
